6449e33c2df3e00c5d960a4347869a19552c5be1
Bartlomiej Lukaszuk <bartek.lukaszuk@o2.pl>
2025-02-21 22:28:22 +0100

    cosmetic changes

--- code_snippets/prime_numbers/prime_numbers.jl
+++ code_snippets/prime_numbers/prime_numbers.jl
@@ -4,11 +4,11 @@ const Vec = Vector
 # https://en.wikipedia.org/wiki/Prime_number#Trial_division
 function isPrime(n::Int)::Bool
     @assert n > 1 "n must be > 1"
+    if n < 4
+        return true
+    end
     upto::Int = round(Int, ceil(sqrt(n)))
     for i in 2:upto
-        if n == 2
-            return true
-        end
         if n % i == 0
             return false
         end
@@ -16,32 +16,28 @@ function isPrime(n::Int)::Bool
     return true
 end
 
-function getPrimes(maxIncl::Int)::Vec{Int}
+function getPrimesV1(maxIncl::Int)::Vec{Int}
     @assert maxIncl >= 2 "maxIncl must be >= 2"
-    primes::Vec{Int} = []
-    for i in 2:maxIncl
-        if isPrime(i)
-            push!(primes, i)
-        end
-    end
-    return primes
+    return filter(isPrime, 2:maxIncl)
 end
 
 # version 2: sieve of Eratosthenes
 # https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
-function getPrimes(maxIncl::Int)
-    @assert maxIncl >= 2 "maxIncl must be >= 2"
+function getXmultiples(x::Int, upto::Int)::StepRange{Int, Int}
+    return (x*2):x:upto
+end
+
+function getPrimesV2(maxIncl::Int)
+    @assert maxIncl > 1 "maxIncl must be > 1"
     maybePrimes::Vec{Bool} = ones(Bool, maxIncl)
     nums::Vec{Int} = 1:maxIncl |> collect
     maybePrimes[1] = false # first prime is: 2
     for num in 2:maxIncl
-        for multiple in (num*2):num:maxIncl
-            maybePrimes[multiple] = false
-        end
+        maybePrimes[getXmultiples(num, maxIncl)] .= false
     end
     return nums[maybePrimes]
 end
 
-primesFromWiki = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53,
-                  59, 61, 67, 71, 73, 79, 83, 89, 97]
-getPrimes(100) == primesFromWiki
+primesFromWiki = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+getPrimesV1(100) == primesFromWiki
+getPrimesV2(100) == primesFromWiki
