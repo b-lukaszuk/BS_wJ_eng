987def83729e3316001ca44b400bbcc55a95df3a
Bartlomiej Lukaszuk <bartek.lukaszuk@o2.pl>
2025-05-29 22:01:41 +0200

    improved getFmtMonth

--- code_snippets/calendar/calendar.jl
+++ code_snippets/calendar/calendar.jl
@@ -53,11 +53,29 @@ reshapeVec(x, Int(length(x) / 7), 7, true)
 x = getDaysInRectangle(30, 1)
 reshapeVec(x, Int(length(x) / 7), 7, true)
 
+function center(sth::A, totLen::Int)::Str where A<:Union{Int, Str}
+    s::Str = string(sth)
+    len::Int = length(s)
+    @assert totLen > 0 && len > 0 "both totLen and len must be > 0"
+    @assert totLen >= len "totLen must be >= len"
+    diff::Int = totLen - len
+    leftSpaceLen::Int = round(Int, diff / 2)
+    rightSpaceLen::Int = diff - leftSpaceLen
+    return " " ^ leftSpaceLen * s * " " ^ rightSpaceLen
+end
+
+const months = Dict(
+    1 => "January", 2 => "February", 3 => "March",
+    4 => "April", 5 => "May", 6 => "June", 7 => "July",
+    8 => "August", 9 => "September", 10 => "October",
+    11 => "November", 12 => "December")
+
 # 1 - Sunday, 7 - Saturday
-function getFmtMonth(nDays::Int, firstDay::Int)
+function getFmtMonth(nMonth::Int, nDays::Int, firstDay::Int)::Str
+    @assert 1 <= nMonth <= 12 "nMonth must be in range [1-12]"
     @assert 28 <= nDays <= 31 "nDays must be in range [28-31]"
     @assert 1 <= firstDay <= 7 "firstDay must be in range [1-7]"
-    daysVerbs::Vec{Str} = ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"]
+    daysVerbs::Str = join(["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"], " ")
     days::Vec{Str} = string.(getDaysInRectangle(nDays, firstDay))
     days = replace(days, "0" =>" ")
     m::Matrix{Str} = reshapeVec(days, Int(length(days)/7), 7, true)
@@ -67,5 +85,6 @@ function getFmtMonth(nDays::Int, firstDay::Int)
     for r in eachrow(m)
         result *= fmtRow(r) * "\n"
     end
-    return join(daysVerbs, " ") * "\n" * result
+    return  center(get(months, nMonth, 1), length(daysVerbs)) * "\n" *
+        daysVerbs * "\n" * result
 end
