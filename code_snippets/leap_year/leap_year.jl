10d6ca2f4d7b13e740d21e0e8f438237d98348f8
Bartlomiej Lukaszuk <bartek.lukaszuk@o2.pl>
2025-06-01 21:39:30 +0200

    corrected isLeap

--- code_snippets/leap_year/leap_year.jl
+++ code_snippets/leap_year/leap_year.jl
@@ -1,13 +1,19 @@
+# the Gregorian Calendar was introduced in 1582
+# that year: 4th October, was followed by 15th October
+# before there was the Julian calendar
 function isLeap(yr::Int)::Bool
-    @assert 0 < yr < 4000
-    if yr % 4 != 0
-        return false
-    elseif yr % 25 != 0
-        return true
-    elseif yr % 16 != 0
-        return false
+    @assert 1 <= yr <= 4001
+    divisibleBy4::Bool = yr % 4 == 0
+    divisibleBy100::Bool = yr % 100 == 0
+    divisibleBy400::Bool = yr % 400 == 0
+    gregorianException::Bool = divisibleBy100 && !divisibleBy400
+    if yr <= 1582
+        return divisibleBy4
+    end
+    if divisibleBy4
+        return !gregorianException
     else
-        return true
+        return false
     end
 end
 
