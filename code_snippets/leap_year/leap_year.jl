8ec928a7e93d0897f6fdc5ff69fc6bf5fa696c3f
Bartlomiej Lukaszuk <bartek.lukaszuk@o2.pl>
2025-06-02 20:39:50 +0200

    cosmetic changes in leap_year.jl

--- code_snippets/leap_year/leap_year.jl
+++ code_snippets/leap_year/leap_year.jl
@@ -4,19 +4,32 @@
 function isLeap(yr::Int)::Bool
     @assert 1 <= yr <= 4001
     divisibleBy4::Bool = yr % 4 == 0
-    divisibleBy100::Bool = yr % 100 == 0
-    divisibleBy400::Bool = yr % 400 == 0
-    gregorianException::Bool = divisibleBy100 && !divisibleBy400
-    if yr <= 1582
-        return divisibleBy4
-    end
-    if divisibleBy4
-        return !gregorianException
-    else
+    gregorianException::Bool = (yr % 100 == 0) && (yr % 400 != 0)
+    if !divisibleBy4
         return false
+    else
+        if yr < 1583
+            return true
+        else
+            return !gregorianException
+        end
+    end
+end
+
+function isLeap(yr::Int)::Bool
+    @assert 1 <= yr <= 4001
+    divisibleBy4::Bool = yr % 4 == 0
+    gregorianException::Bool = (yr % 100 == 0) && (yr % 400 != 0)
+    if !divisibleBy4
+        return false
+    else
+        return yr < 1582 ? true : !gregorianException
     end
 end
 
 yrs = [1792, 1859, 1900, 1918, 1974, 1985, 2000, 2012]
 isLeap.(yrs)
 filter(isLeap, yrs)
+# in every 400 years are 303 regular years and 97 leap years
+# (in the Gregorian calendar)
+filter(isLeap, 1583:(1583+400-1)) |> length
