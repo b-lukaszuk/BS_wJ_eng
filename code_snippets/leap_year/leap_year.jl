786600513925816bc37052682f5cf8c94161ab14
Bartlomiej Lukaszuk <bartek.lukaszuk@o2.pl>
2025-06-03 22:09:56 +0200

    cosmetic changes in leap_year.jl

--- code_snippets/leap_year/leap_year.jl
+++ code_snippets/leap_year/leap_year.jl
@@ -2,34 +2,45 @@
 # that year: 4th October, was followed by 15th October
 # before there was the Julian calendar
 function isLeap(yr::Int)::Bool
-    @assert 1 <= yr <= 4001
+    @assert 0 < yr < 4001
     divisibleBy4::Bool = yr % 4 == 0
     gregorianException::Bool = (yr % 100 == 0) && (yr % 400 != 0)
     if !divisibleBy4
         return false
     else
-        if yr < 1583
-            return true
-        else
-            return !gregorianException
-        end
+        return !gregorianException
     end
 end
 
 function isLeap(yr::Int)::Bool
-    @assert 1 <= yr <= 4001
+    @assert 0 < yr < 4001
     divisibleBy4::Bool = yr % 4 == 0
     gregorianException::Bool = (yr % 100 == 0) && (yr % 400 != 0)
-    if !divisibleBy4
-        return false
-    else
-        return yr < 1582 ? true : !gregorianException
-    end
+    return divisibleBy4 && !gregorianException
 end
 
 yrs = [1792, 1859, 1900, 1918, 1974, 1985, 2000, 2012]
 isLeap.(yrs)
 filter(isLeap, yrs)
-# in every 400 years are 303 regular years and 97 leap years
-# (in the Gregorian calendar)
-filter(isLeap, 1583:(1583+400-1)) |> length
+
+# simple test suite
+# for proper testing see: https://docs.julialang.org/en/v1/stdlib/Test/
+
+# in the Gregorian calendar in every 400 years time span
+# there are 303 regular years and 97 leap years
+function runTest()::Int
+    startYr::Int = 0
+    endYr::Int = 0
+    numLeapYrs::Int = 0
+    for i in 1:3601
+        startYr = i
+        endYr = i + 400 - 1
+        numLeapYrs = filter(isLeap, startYr:endYr) |> length
+        if numLeapYrs != 97
+            return 1
+        end
+    end
+    return 0
+end
+
+runTest()
