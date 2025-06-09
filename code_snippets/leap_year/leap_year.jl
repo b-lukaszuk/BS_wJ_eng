ca52b6e48b8bd7c52f602b618e4d05d9da54a8d8
Bartlomiej Lukaszuk <bartek.lukaszuk@o2.pl>
2025-06-09 21:14:32 +0200

    added some simp. tests to code_snippets/leap_year/

--- code_snippets/leap_year/leap_year.jl
+++ code_snippets/leap_year/leap_year.jl
@@ -1,3 +1,5 @@
+import Test
+
 # the Gregorian Calendar was introduced in 1582
 # that year: 4th October, was followed by 15th October
 # before there was the Julian calendar
@@ -5,28 +7,13 @@ function isLeap(yr::Int)::Bool
     @assert 0 < yr < 4001 "yr must be in range [1-4000]"
     divisibleBy4::Bool = yr % 4 == 0
     gregorianException::Bool = (yr % 100 == 0) && (yr % 400 != 0)
-    if !divisibleBy4
-        return false
-    else
-        return !gregorianException
-    end
-end
-
-function isLeap(yr::Int)::Bool
-    @assert 0 < yr < 4001 "yr must be in range [1-4000]"
-    divisibleBy4::Bool = yr % 4 == 0
-    gregorianException::Bool = (yr % 100 == 0) && (yr % 400 != 0)
     return divisibleBy4 && !gregorianException
 end
 
-yrs = [1792, 1859, 1900, 1918, 1974, 1985, 2000, 2012]
-filter(isLeap, yrs)
-
-# simple test suite
-# for proper testing see: https://docs.julialang.org/en/v1/stdlib/Test/
 
 # in the Gregorian calendar in every 400 years time span
 # there are 303 regular years and 97 leap years
+# very simplistic unit testing
 function runTestSet()::Int
     startYr::Int = 0
     endYr::Int = 0
@@ -43,3 +30,18 @@ function runTestSet()::Int
 end
 
 runTestSet()
+
+
+# slightly more correct testing
+# but not quite, see: https://discourse.julialang.org/t/best-practices-for-julia-unit-testing/30858
+Test.@testset "97 leap years per 400-year period" begin
+    startYr::Int = 0
+    endYr::Int = 0
+    numLeapYrs::Int = 0
+    for i in 1:3601
+        startYr = i
+        endYr = i + 400 - 1
+        numLeapYrs = filter(isLeap, startYr:endYr) |> length
+        Test.@test numLeapYrs == 97
+    end
+end
