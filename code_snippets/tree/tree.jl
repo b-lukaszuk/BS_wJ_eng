9475b1c7037a5ce909961162f0c6e32bb2ad3ae7
Bartlomiej Lukaszuk <bartek.lukaszuk@o2.pl>
2025-06-28 23:45:46 +0200

    cosmetic changes

--- code_snippets/tree/tree.jl
+++ code_snippets/tree/tree.jl
@@ -6,12 +6,13 @@ function printCatalogTree(path::Str, level::Int=0)
     for name in readdir(path)
         newPath::Str = joinpath(path, name)
         if isfile(newPath)
-            println("  " ^ level * name)
+           println(" " ^ level * "|-" * "-" ^ level * name)
         else
-            println("  " ^ level * name * "/")
+            println(" " ^ level * "|-" * "-" ^ level * name * "/")
             printCatalogTree(newPath, level+1)
         end
     end
+    return nothing
 end
 
 printCatalogTree(startDir)
