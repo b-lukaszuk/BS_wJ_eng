f0d87942f372477a3a6020f740793f80ade5ae58
Bartlomiej Lukaszuk <bartek.lukaszuk@o2.pl>
2025-06-29 20:48:48 +0200

    corrected printCatalogTree in tree.jl

--- code_snippets/tree/tree.jl
+++ code_snippets/tree/tree.jl
@@ -2,16 +2,23 @@ const Str = String
 
 startDir = joinpath(homedir(), "Desktop", "catalog_a")
 
-function printCatalogTree(path::Str, level::Int=0)
+function printCatalogTree(path::Str, pad::Str)
     for name in readdir(path)
         newPath::Str = joinpath(path, name)
+        newPad::Str = pad * "---"
         if isfile(newPath)
-           println(" " ^ level * "|-" * "-" ^ level * name)
+           println(newPad * name)
         else
-            println(" " ^ level * "|-" * "-" ^ level * name * "/")
-            printCatalogTree(newPath, level+1)
+            println(newPad * name * "/")
+            printCatalogTree(newPath, pad * "   |")
         end
     end
+    return nothing
+end
+
+function printCatalogTree(path::Str)
+    println(path)
+    printCatalogTree(path, "|")
     return nothing
 end
 
