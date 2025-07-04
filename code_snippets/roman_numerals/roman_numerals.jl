19e0d660bedc3fb077c7346a83160d9bf2a891d0
Bartlomiej Lukaszuk <bartek.lukaszuk@o2.pl>
2025-07-04 18:49:26 +0200

    cosmetic changes in roman_numerals.jl

--- code_snippets/roman_numerals/roman_numerals.jl
+++ code_snippets/roman_numerals/roman_numerals.jl
@@ -1,13 +1,21 @@
 const Str = String
 const Vec = Vector
 
-roman2arabic::Vec{Tuple{Str, Int}} = [
-    ("M", 1000), ("CM", 900),
-    ("D", 500), ("CD", 400),
-    ("C", 100), ("XC", 90),
-    ("L", 50), ("XL", 40),
-    ("X", 10), ("IX", 9), ("V", 5), ("IV", 4), ("I", 1)
-]
+roman2arabic = [("M", 1000), ("CM", 900),
+                ("D", 500), ("CD", 400),
+                ("C", 100), ("XC", 90),
+                ("L", 50), ("XL", 40),
+                ("X", 10), ("IX", 9), ("V", 5),
+                ("IV", 4), ("I", 1)]
+romanTokens = map(first, roman2arabic)
+arabicTest = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+              11, 12, 39, 246, 789, 2421, 160, 207, 1009, 1066,
+              3999, 1776, 1918, 1944, 2025]
+romanTest = ["I", "II", "III", "IV", "V",
+             "VI", "VII", "VIII", "IX", "X",
+             "XI", "XII", "XXXIX", "CCXLVI", "DCCLXXXIX",
+             "MMCDXXI", "CLX", "CCVII", "MIX", "MLXVI",
+             "MMMCMXCIX", "MDCCLXXVI", "MCMXVIII", "MCMXLIV", "MMXXV"]
 
 function getRoman(arabic::Int)::Str
     @assert 0 < arabic < 4000
@@ -21,20 +29,9 @@ function getRoman(arabic::Int)::Str
     return roman
 end
 
-arabicTest = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-              11, 12, 39, 246, 789, 2421, 160, 207, 1009, 1066,
-              3999, 1776, 1918, 1944, 2025]
-romanTest = ["I", "II", "III", "IV", "V",
-             "VI", "VII", "VIII", "IX", "X",
-             "XI", "XII", "XXXIX", "CCXLVI", "DCCLXXXIX",
-             "MMCDXXI", "CLX", "CCVII", "MIX", "MLXVI",
-             "MMMCMXCIX", "MDCCLXXVI", "MCMXVIII", "MCMXLIV", "MMXXV"]
-
 getRoman.(arabicTest) == romanTest
 
-romanTokens = map(first, roman2arabic)
-
-function getToken(roman::Str)::Tuple{Str, Str}
+function getTokenAndRest(roman::Str)::Tuple{Str, Str}
     if length(roman) <= 1
         return (roman, "")
     elseif roman[1:2] in romanTokens
@@ -48,18 +45,26 @@ function getTokens(roman::Str)::Vec{Str}
     token::Str = ""
     tokens::Vec{Str} = []
     while (roman != "")
-        token, roman = getToken(roman)
+        token, roman = getTokenAndRest(roman)
         push!(tokens, token)
     end
     return tokens
 end
 
+function getVal(lookup::Vec{Tuple{Str, Int}}, key::Str, default::Int=0)::Int
+    for (k, v) in lookup
+        if k == key
+            return v
+        end
+    end
+    return default
+end
+
 function getArabic(roman::Str)::Int
     tokens::Vec{Str} = getTokens(roman)
     sum::Int = 0
-    lookup::Dict{Str, Int} = Dict(roman2arabic)
     for token in tokens
-        sum += get(lookup, token, 0)
+        sum += getVal(roman2arabic, token, 0)
     end
     return sum
 end
