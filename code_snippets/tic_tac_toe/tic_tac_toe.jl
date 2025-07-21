f1ffc7c393c67db8d54d60d4aec1a2aa26217fb6
Bartlomiej Lukaszuk <bartek.lukaszuk@o2.pl>
2025-07-21 15:06:09 +0200

    added part of solution to tic_tac_toe.jl

--- code_snippets/tic_tac_toe/tic_tac_toe.jl
+++ code_snippets/tic_tac_toe/tic_tac_toe.jl
@@ -1,2 +1,28 @@
 # TODO:
 # Write a simple tic-tac-toe game
+
+# arr 1-9 is field index to display
+# 100 is 'x'
+# 1_000 is 'o'
+grid = [1 2 3;
+        4 5 6;
+        7 8 9]
+
+function getField(n::Int)::Char
+    if n == 100
+        return 'x'
+    elseif n == 1_000
+        return 'o'
+    else
+        return string(n)[1]
+    end
+end
+
+function printGrid(g::Array{Int, 2})
+    for r in eachrow(g)
+        println(join(map(getField, r), "  "))
+    end
+    return nothing
+end
+
+printGrid(grid)
