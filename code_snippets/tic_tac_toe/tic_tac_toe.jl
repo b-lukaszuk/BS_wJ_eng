e48938d3d9740760e58f86910d51990767531c4d
Bartlomiej Lukaszuk <bartek.lukaszuk@o2.pl>
2025-07-29 22:12:55 +0200

    changed board representation, printing, etc. in tic_tac_toe.jl

--- code_snippets/tic_tac_toe/tic_tac_toe.jl
+++ code_snippets/tic_tac_toe/tic_tac_toe.jl
@@ -3,42 +3,56 @@
 const Str = String
 const Vec = Vector
 
-# arr 1-9 is field index to display
-# 100 is 'x'
-# 1_000 is 'o'
-grid = [1 2 3;
-        4 5 6;
-        7 8 9]
-
-function getField(n::Int)::Str
-    if n == 100
-        return "x"
-    elseif n == 1_000
-        return "o"
-    else
-        # https://en.wikipedia.org/wiki/ANSI_escape_code#Colors
-        # "\x1b[90m" - FG light black (gray)
-        # "\x1b[0m" - reset color to default
-        return "\x1b[90m" * string(n) * "\x1b[0m"
-    end
+function getNewGameBoard()::Vec{Str}
+    return string.(1:9)
 end
 
-function printGrid(g::Array{Int, 2})
-    for (rowIndex, row) in enumerate(eachrow(g))
-        println(" " * join(map(getField, row), " | "))
-        if rowIndex < 3
-            println("---+---+---")
-        end
+board = getNewGameBoard()
+
+# https://en.wikipedia.org/wiki/ANSI_escape_code#Colors
+function getGray(s::Str)::Str
+    return "\x1b[90m" * s * "\x1b[0m"
+end
+
+function getDefault(s::Str)::Str
+    return "\x1b[0m" * s
+end
+
+function getRed(s::Str)::Str
+    return "\x1b[31m" * s * "\x1b[0m"
+end
+
+function colorBoard(board::Vec{Str})::Vec{Str}
+    result::Vec{Str} = copy(board)
+    for i in 1:9
+        result[i] = board[i] in ["X", "O"] ?
+            getDefault(board[i]) : getGray(board[i])
     end
+    return result
+end
+
+function clearLines(nLines::Int)
+    @assert 0 < nLines "nLines must be a positive integer"
+    #"\033[xxxA" - xxx moves cursor up xxx lines
+    print("\033[" * string(nLines) * "A")
+    # clear from cursor position till the end of the screen
+    print("\033[0J")
+end
+
+function printBoard(board::Vec{Str})
+    cb::Vec{Str} = colorBoard(board)
+    for r in [1, 4, 7]
+        println(" ", join(cb[r:(r+2)], " | "))
+        println("---+---+---")
+    end
+    clearLines(1)
     return nothing
 end
 
-printGrid(grid)
-
-grid = [1 100 3;
-        4 5 6;
-        1_000 1_000 9]
-printGrid(grid)
+printBoard(board)
+board[1] = "O"
+board[5] = "X"
+printBoard(board)
 
 function getUserInput(prompt::Str)::Str
     print(prompt)
@@ -64,13 +78,6 @@ isMoveLegal("3.0", grid)
 isMoveLegal("3", grid)
 isMoveLegal("5 ", grid)
 
-function clearLines(nLines::Int)
-    @assert 0 < nLines "nLines must be a positive integer"
-    #"\033[xxxA" - xxx moves cursor up xxx lines
-    print("\033[" * string(nLines) * "A")
-    # clear from cursor position till the end of the screen
-    print("\033[0J")
-end
 
 function getUserMove(gameBoard::Array{Int, 2})::Int
     input::Str = getUserInput("Enter your move: ")
