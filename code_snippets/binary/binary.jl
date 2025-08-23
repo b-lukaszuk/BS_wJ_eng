4e4e59483a229a691121dcaec5f2b3a7a9fb5700
Bartlomiej Lukaszuk <bartek.lukaszuk@o2.pl>
2025-08-23 18:01:53 +0200

    added part of solutio to binary.jl

--- code_snippets/binary/binary.jl
+++ code_snippets/binary/binary.jl
@@ -1,1 +1,30 @@
 # TODO: the solution goes here
+
+const Str = String
+
+# built-ins
+# decimal to binary
+string(113, base=2)
+# binary to decimal
+parse(Int, string(113, base=2), base=2)
+
+
+# fmt from overpayment, modified
+function fmtBin(binNum::Str)::Str
+    result::Str = ""
+    counter::Int = 0
+    padLen::Int = length(binNum) % 4
+    padLen = padLen == 0 ? padLen : 4 - padLen
+    for binDigit in reverse(binNum) # binDigit is a single binDigit (type Char)
+        if counter == 4
+            result = " " * result
+            counter = 0
+        end
+        result = binDigit * result
+        counter += 1
+    end
+    return "0" ^ padLen * result
+end
+
+string(15, base=2) |> fmtBin
+string(113, base=2) |> fmtBin
