e2716d6879e3dc7507481d8dbd978b118a4f8682
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2025-09-23 14:50:48 +0200

    cosmetic changes in lattice_paths.jl

--- code_snippets/lattice_paths/lattice_paths.jl
+++ code_snippets/lattice_paths/lattice_paths.jl
@@ -1,5 +1,7 @@
 const Vec = Vector
-const Tup = Tuple
+
+const Pos = Tuple{Int, Int}
+const Path = Vec{Pos}
 
 const RIGHT = (1, 0)
 const DOWN = (0, -1)
@@ -8,13 +10,13 @@ const DOWN = (0, -1)
 # and it may not act correctly
 
 # https://projecteuler.net/problem=15
-function add(position::Tup{Int, Int}, move::Tup{Int, Int})::Tup{Int, Int}
+function add(position::Pos, move::Pos)::Pos
     return (position[1]+move[1], position[2]+move[2])
 end
 
-function add(positions::Vec{Tup{Int, Int}},
-             moves::Vec{Tup{Int, Int}})::Vec{Tup{Int, Int}}
-    result::Vec{Tup{Int, Int}} = []
+function add(positions::Vec{Pos},
+             moves::Vec{Pos})::Vec{Pos}
+    result::Vec{Pos} = []
     for p in positions
         for m in moves
             push!(result, add(p, m))
@@ -23,18 +25,17 @@ function add(positions::Vec{Tup{Int, Int}},
     return result
 end
 
-function getSums(nRows::Int=2)::Vec{Tup{Int, Int}}
-    @assert 0 < nRows < 6 "nRows must be in the range [1-5]"
-    sums::Vec{Tup{Int, Int}} = [(0, 0)]
-    moves::Vec{Tup{Int, Int}} = [RIGHT, DOWN]
+function getSums(nRows::Int=2)::Vec{Pos}
+    @assert 0 < nRows < 5 "nRows must be in the range [1-4]"
+    sums::Vec{Pos} = [(0, 0)]
+    moves::Vec{Pos} = [RIGHT, DOWN]
     for _ in 1:(nRows*2) # - *2 - because of columns
         sums = add(sums, moves)
     end
     return sums
 end
 
-function isEqlTarget(position::Tup{Int, Int},
-                     target::Tup{Int, Int})::Bool
+function isEqlTarget(position::Pos, target::Pos)::Bool
     return position == target
 end
 
@@ -50,25 +51,23 @@ s = getSums(4)
 filter(y -> isEqlTarget(y, (4, -4)), s) |> length
 binomial(8, 4)
 
-s = getSums(5)
-filter(y -> isEqlTarget(y, (5, -5)), s) |> length
-binomial(10, 5)
-
-function makeOneStep(prevPaths::Vec{Vec{Tup{Int, Int}}})::Vec{Vec{Tup{Int, Int}}}
+function makeOneStep(prevPaths::Vec{Path})::Vec{Path}
     @assert !isempty(prevPaths) "prevPaths cannot be empty"
-    result::Vec{Vec{Tup{Int, Int}}} = []
+    result::Vec{Path} = []
+    step1::Pos = (0, 0)
+    step2::Pos = (0, 0)
     for path in prevPaths
-        step1::Tup{Int, Int} = add(path[end], RIGHT)
+        step1 = add(path[end], RIGHT)
         push!(result, [path..., step1])
-        step2::Tup{Int, Int} = add(path[end], DOWN)
+        step2 = add(path[end], DOWN)
         push!(result, [path..., step2])
     end
     return result
 end
 
-function getPaths(nRows::Int=2)::Vec{Vec{Tup{Int, Int}}}
-    @assert 0 < nRows < 6 "nRows must be in the range [1-5]"
-    result::Vec{Vec{Tup{Int, Int}}} = [[(0, 0)]]
+function getPaths(nRows::Int=2)::Vec{Path}
+    @assert 0 < nRows < 5 "nRows must be in the range [1-4]"
+    result::Vec{Path} = [[(0, 0)]]
     for _ in 1:(nRows*2) # - *2 - because of columns
         result = makeOneStep(result)
     end
@@ -82,3 +81,7 @@ binomial(4, 2)
 ps = getPaths(3)
 ps = filter(v -> v[end] == (3, -3), ps)
 binomial(6, 3)
+
+ps = getPaths(4)
+ps = filter(v -> v[end] == (4, -4), ps)
+binomial(8, 4)
