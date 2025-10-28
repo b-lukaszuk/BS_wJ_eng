4ec6e773501fcfe1ba1632e49cb82e1c25ee5bfc
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2025-10-28 22:06:37 +0100

    cosmetic changes in birthday.jl

--- code_snippets/birthday/birthday.jl
+++ code_snippets/birthday/birthday.jl
@@ -1,4 +1,4 @@
-import Random as Rand
+import Random as Rnd
 
 const Flt = Float64
 const Str = String
@@ -6,6 +6,11 @@ const Vec = Vector
 
 # the code in this file is meant to serve as a programming exercise only
 # and it may not act correctly
+
+function getBdaysAtParty(nPeople::Int)::Vec{Int}
+    @assert 3 < nPeople < 366 "nPeople must be in range [4-365]"
+    return Rnd.rand(1:365, nPeople)
+end
 
 function getCounts(v::Vector{T})::Dict{T,Int} where T
     counts::Dict{T,Int} = Dict()
@@ -15,48 +20,31 @@ function getCounts(v::Vector{T})::Dict{T,Int} where T
     return counts
 end
 
-function getBdaysAtParty(nPeople::Int)::Vec{Int}
-    @assert 3 < nPeople < 366 "nPeople must be in range [4-365]"
-    return Rand.rand(1:365, nPeople)
+function anySameBdays(counts::Dict{Int, Int})::Bool
+    return isnothing(findfirst((>)(1), counts)) ? false : true
 end
 
-function areAnyBornSameDay(nPeople::Int)::Bool
-    @assert 3 < nPeople < 366 "nPeople must be in range [4-365]"
-    peopleBdays::Vec{Int} = getBdaysAtParty(nPeople)
-    counts::Dict{Int, Int} = getCounts(peopleBdays)
-    sameBday::Union{Int, Nothing} = findfirst((>)(1), counts)
-    return isnothing(sameBday) ? false : true
-end
-
-function getProbAny2SameBdays(nPeople::Int, nSimulations::Int=100_000)::Flt
-    @assert 3 < nPeople < 366 "nPeople must be in range [4-365]"
-    @assert 1e4 <= nSimulations <= 1e6 "nSimulations not in range [1e4-1e6]"
-    sameBdays::Vec{Bool} = Vec{Bool}(undef, nSimulations)
-    for i in 1:nSimulations
-        sameBdays[i] = areAnyBornSameDay(nPeople)
-    end
-    return sum(sameBdays)/nSimulations
-end
-
-function anyBdayEqMyBday(nPeople::Int, myBday::Int=1)::Bool
-    @assert 3 < nPeople < 366 "nPeople must be in range [4-365]"
-    peopleBdays::Vec{Int} = getBdaysAtParty(nPeople)
-    counts::Dict{Int, Int} = getCounts(peopleBdays)
+function anyMyBday(counts::Dict{Int, Int}, myBday::Int=1)::Bool
     return haskey(counts, myBday)
 end
 
-function getProbMyBday(nPeople::Int, myBday::Int=1, nSimulations::Int=1000_000)::Flt
+# isEventFn(Dict{Int, Int}) -> Bool
+function getProbSuccess(nPeople::Int, isEventFn::Function,
+                        nSimulations::Int=100_000)::Flt
     @assert 3 < nPeople < 366 "nPeople must be in range [4-365]"
     @assert 1e4 <= nSimulations <= 1e6 "nSimulations not in range [1e4-1e6]"
-    sameMyBdays::Vec{Bool} = Vec{Bool}(undef, nSimulations)
+    successes::Vec{Bool} = Vec{Bool}(undef, nSimulations)
     for i in 1:nSimulations
-        sameMyBdays[i] = anyBdayEqMyBday(nPeople, myBday)
+        peopleBdays::Vec{Int} = getBdaysAtParty(nPeople)
+        counts::Dict{Int, Int} = getCounts(peopleBdays)
+        eventOccured::Bool = isEventFn(counts)
+        successes[i] = eventOccured
     end
-    return sum(sameMyBdays)/nSimulations
+    return sum(successes)/nSimulations
 end
 
-# getProbAny2SameBdays(23, N_SIMULATIONS)
-Rand.seed!(303)
-probs = map(getProbMyBday, 5:50)
-probs = map(getProbAny2SameBdays, 4:50)
-println(probs)
+# running a simulation may take a while
+# reducing nSimulations will shorten the time and lower the precision of prob. estimation
+Rnd.seed!(101)
+probsAnySameBdays = Dict(i => getProbSuccess(i, anySameBdays) for i in 5:30)
+probsMyBday = Dict(i => getProbSuccess(i, anyMyBday) for i in 5:30)
