8efec80458270b4a0096d890c5953ec604c963a2
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2025-10-28 18:16:59 +0100

    added part of solution to birthday.jl

--- code_snippets/birthday/birthday.jl
+++ code_snippets/birthday/birthday.jl
@@ -9,6 +9,7 @@ const Vec = Vector
 
 const BIRTHDAYS = 1:365
 const N_SIMULATIONS = 100_000
+const MY_BDAY = 1
 
 function getCounts(v::Vector{T})::Dict{T,Int} where T
     counts::Dict{T,Int} = Dict()
@@ -41,7 +42,25 @@ function getProbAny2SameBdays(nPeopleAtParty::Int, nSimulations::Int=N_SIMULATIO
     return sum(sameBdays)/nSimulations
 end
 
+function anyBdayEqMyBday(nPeopleAtParty::Int, myBday::Int = MY_BDAY)::Bool
+    @assert nPeopleAtParty > 3 "at least 4 people must be at a party"
+    peopleBdays::Vec{Int} = getBdaysOfPeopleAtParty(nPeopleAtParty)
+    counts::Dict{Int, Int} = getCounts(peopleBdays)
+    return haskey(counts, myBday)
+end
+
+function getProbMyBday(nPeopleAtParty::Int, myBday::Int=MY_BDAY, nSimulations::Int=N_SIMULATIONS)::Flt
+    @assert nPeopleAtParty > 3 "at least 4 people must be at a party"
+    @assert 10_000 <= nSimulations <= 1_000_000 "nSimulations must be in range [1e4-1e6]"
+    sameMyBdays::Vec{Bool} = Vec{Bool}(undef, nSimulations)
+    for i in 1:nSimulations
+        sameMyBdays[i] = anyBdayEqMyBday(nPeopleAtParty-1, myBday)
+    end
+    return sum(sameMyBdays)/nSimulations
+end
+
 # getProbAny2SameBdays(23, N_SIMULATIONS)
 Rand.seed!(303)
+probs = map(getProbMyBday, 5:50)
 probs = map(getProbAny2SameBdays, 4:50)
 println(probs)
