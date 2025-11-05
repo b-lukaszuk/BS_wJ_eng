5da09cfa5bb2d5e2262546fd5cc668a63ba0c862
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2025-11-05 19:38:04 +0100

    randomness.jl added link (comment) to wiki.LCG

--- code_snippets/randomness/randomness.jl
+++ code_snippets/randomness/randomness.jl
@@ -1,3 +1,5 @@
 # todo
 # https://en.wikipedia.org/wiki/Pseudorandom_number_generator
 # https://en.wikipedia.org/wiki/List_of_random_number_generators#Pseudorandom_number_generators_(PRNGs)
+
+# https://en.wikipedia.org/wiki/Linear_congruential_generator
