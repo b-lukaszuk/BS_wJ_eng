dbb6993eeaf4de17ef173ccf23aa1464746c0a0a
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2025-11-06 19:58:02 +0100

    added part of solution to randomness.jl

--- code_snippets/randomness/randomness.jl
+++ code_snippets/randomness/randomness.jl
@@ -4,3 +4,25 @@
 
 # https://en.wikipedia.org/wiki/Linear_congruential_generator
 # https://en.wikipedia.org/wiki/Box%E2%80%93Muller_transform
+
+
+# the code in this file is meant to serve as a programming exercise only
+# it may not act correctly
+
+import Dates as Dt
+
+a = 1664525
+c = 1013904223
+m = 2^32
+seed = round(Int, Dt.now() |> Dt.datetime2unix)
+
+function setSeed!(newSeed)
+    global seed = newSeed
+    return nothing
+end
+
+function getRand()::Int
+    newSeed::Int = (a * seed + c) % m
+    setSeed!(newSeed)
+    return newSeed
+end
