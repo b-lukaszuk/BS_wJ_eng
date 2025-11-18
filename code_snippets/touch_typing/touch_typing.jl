d81a10bc07ad725fd7d7757b885325db950e02e5
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2025-11-18 16:51:40 +0100

    added part of solution to touch_typing.jl

--- code_snippets/touch_typing/touch_typing.jl
+++ code_snippets/touch_typing/touch_typing.jl
@@ -1,2 +1,34 @@
 # the code in this file is meant to serve as a programming exercise only
 # it may not act correctly
+
+const Str = String
+
+const TXT = "julia is awsome"
+
+# https://en.wikipedia.org/wiki/ANSI_escape_code#Colors
+function getRed(s::Str)::Str
+    # "\x1b[31m" sets forground color to red
+    # "\x1b[0m" resets forground color to default value
+    return "\x1b[31m" * s * "\x1b[0m"
+end
+
+function getGreen(s::Str)::Str
+    # "\x1b[32m" sets forground color to red
+    # "\x1b[0m" resets forground color to default value
+    return "\x1b[32m" * s * "\x1b[0m"
+end
+
+function getColoredTxt(typedTxt::Str, referenceTxt::Str=TXT)::Str
+    result::Str = ""
+    len::Int = length(typedTxt)
+    for i in eachindex(referenceTxt)
+        if i > len
+            result *= referenceTxt[i]
+        elseif typedTxt[i] == referenceTxt[i]
+            result *= getGreen(string(referenceTxt[i]))
+        else
+            result *= getRed(string(referenceTxt[i]))
+        end
+    end
+    return result
+end
