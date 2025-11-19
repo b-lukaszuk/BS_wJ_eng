76efae93b26b06ff3f87b4711157a1b914ac6cb7
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2025-11-19 18:53:39 +0100

    cosmetic changes in touch_typing.jl

--- code_snippets/touch_typing/touch_typing.jl
+++ code_snippets/touch_typing/touch_typing.jl
@@ -7,14 +7,10 @@ const Vec = Vector
 
 # https://en.wikipedia.org/wiki/ANSI_escape_code#Colors
 function getRed(s::Str)::Str
-    # "\x1b[31m" sets forground color to red
-    # "\x1b[0m" resets forground color to default value
     return "\x1b[31m" * s * "\x1b[0m"
 end
 
 function getGreen(s::Str)::Str
-    # "\x1b[32m" sets forground color to red
-    # "\x1b[0m" resets forground color to default value
     return "\x1b[32m" * s * "\x1b[0m"
 end
 
@@ -36,9 +32,7 @@ end
 # https://en.wikipedia.org/wiki/ANSI_escape_code
 function clearLines(nLines::Int=1)
     @assert 0 < nLines "nLines must be a positive integer"
-    # "\033[xxxA" - xxx moves cursor up xxx LINES
     print("\033[", nLines, "A")
-    # "\033[0J" - clears from cursor position till the end of the screen
     print("\033[0J")
     return nothing
 end
@@ -61,7 +55,7 @@ function playTypingGame(text2beTyped::Str)::Str
     while length(text2beTyped) > length(typedTxt)
         clearLines()
         println(getColoredTxt(typedTxt, text2beTyped), "\r")
-        c = read(stdin, Char)  # read a character without Enter
+        c = read(stdin, Char) # read a character without Enter
         if isDelete(c)
             typedTxt = typedTxt[1:(end-1)]
         elseif isAbort(c)
@@ -72,17 +66,23 @@ function playTypingGame(text2beTyped::Str)::Str
     end
     clearLines()
     println(getColoredTxt(typedTxt, text2beTyped), "\r")
-    run(`stty cooked echo`) # reset terminal default
+    run(`stty cooked echo`) # reset to default behavior
     return typedTxt
 end
 
 function getAccuracy(typedTxt::Str, text2beTyped::Str)::Flt
-    len::Int = length(typedTxt)
-    correctlyTyped::Vec{Bool} = collect(typedTxt) .== collect(text2beTyped)
-    return sum(correctlyTyped) / len
+    len1::Int = length(typedTxt)
+    len2::Int = length(text2beTyped)
+    correctlyTyped::Vec{Bool} = zeros(Bool, len2)
+    for i in 1:len1
+        correctlyTyped[i] = typedTxt[i] == text2beTyped[i]
+    end
+    return sum(correctlyTyped) / len1
 end
 
 function printSummary(typedTxt::Str, text2beTyped::Str, elapsedTimeSec::Flt)
+    len1::Int = length(typedTxt)
+    len2::Int = length(text2beTyped)
     wordLen::Int = 4
     secsPerMin::Int = 60
     txtLen::Int = length(typedTxt)
@@ -91,6 +91,7 @@ function printSummary(typedTxt::Str, text2beTyped::Str, elapsedTimeSec::Flt)
     acc::Flt = getAccuracy(typedTxt, text2beTyped)
     println("\n---Summary---")
     println("Elapsed time: ", round(elapsedTimeSec, digits=2), " seconds")
+    println("Typed characters: $len1/$len2")
     println("Characters per minute: ", round(cpm, digits=1))
     println("Words per minute: ", round(wpm, digits=1))
     println("Accuracy: ", round(acc * 100, digits=2), "%")
@@ -99,15 +100,16 @@ end
 
 function main()
 
-    txt2type = "Julia is awesome. Try it out in 2025, try it out later!"
-
     println("Hello. This is a toy program for touch typing.")
-    println("To work correctly your terminal must support ANSI escape codes.\n")
+    println("It should work well on terminals that support ANSI escape codes.\n")
 
     println("Press Enter (or any key and Enter) and start typing.")
-    println("Press q to quit.")
+    println("Or press q and Enter to quit now.")
+
     choice::Str = readline()
+    txt2type = "Julia is awesome. Try it out in 2025 and beyond!"
     typedTxt::Str = ""
+
     if lowercase(strip(choice)) != "q"
         timeStart::Flt = time()
         typedTxt = playTypingGame(txt2type)
