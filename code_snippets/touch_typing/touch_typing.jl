ac41410df7be76582baeb0c3ed1bffb75d095217
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2025-11-19 16:19:30 +0100

    cosmetic changes in touch_typing.jl

--- code_snippets/touch_typing/touch_typing.jl
+++ code_snippets/touch_typing/touch_typing.jl
@@ -3,7 +3,7 @@
 
 const Str = String
 
-const TXT = "julia is awesome"
+const TXT = "Julia is awesome. Try it out in 2025, try it out later!"
 
 # https://en.wikipedia.org/wiki/ANSI_escape_code#Colors
 function getRed(s::Str)::Str
@@ -34,7 +34,7 @@ function getColoredTxt(typedTxt::Str, referenceTxt::Str=TXT)::Str
 end
 
 # https://en.wikipedia.org/wiki/ANSI_escape_code
-function clearLines(nLines::Int)
+function clearLines(nLines::Int=1)
     @assert 0 < nLines "nLines must be a positive integer"
     # "\033[xxxA" - xxx moves cursor up xxx LINES
     print("\033[", nLines, "A")
@@ -46,21 +46,26 @@ end
 # more info on stty, type in the terminal: man stty
 # display current stty settings with: stty -a (or: stty --all)
 function playTypingGame()
-    println("Start typing the text:")
     c::Char = ' '
     typedTxt::Str = ""
     run(`stty raw -echo`) # raw mode - reads single character immediately
+    println("\n")
     while length(TXT) > length(typedTxt)
-        clearLines(1)
-        println(getColoredTxt(typedTxt))
-        print("\r")
+        clearLines()
+        println(getColoredTxt(typedTxt), "\r")
         c = read(stdin, Char)  # read a character without Enter
-        typedTxt *= c
+        if c == '\x08' || c == '\x7F'
+            typedTxt = typedTxt[1:(end-1)]
+        elseif c == '\x03' || c == '\x04'
+            break
+        else
+            typedTxt *= c
+        end
     end
-    clearLines(1)
-    println(getColoredTxt(typedTxt))
-    print("\r")
+    clearLines()
+    println(getColoredTxt(typedTxt), "\r")
     run(`stty cooked echo`) # reset terminal default
+    return nothing
 end
 
 playTypingGame()
