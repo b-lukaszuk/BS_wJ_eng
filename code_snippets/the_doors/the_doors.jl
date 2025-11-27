ee9971a43f4d5fdb7990c537e7f252cfebebf63f
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2025-11-27 18:12:14 +0100

    added part of solution to the_doors.jl

--- code_snippets/the_doors/the_doors.jl
+++ code_snippets/the_doors/the_doors.jl
@@ -1,5 +1,53 @@
+# https://en.wikipedia.org/wiki/Monty_Hall_problem
+import Random as Rnd
+
 # the code in this file is meant to serve as a programming exercise only
 # it may not act correctly
+#
+const Flt = Float64
+const Vec = Vector
 
-# TODO
-# https://en.wikipedia.org/wiki/Monty_Hall_problem
+mutable struct Door
+    isCar::Bool
+    isChosen::Bool
+    isOpen::Bool
+end
+
+function get3RandDoors()::Vec{Door}
+    cars::Vec{Bool} = Rnd.shuffle([true, false, false])
+    chosen::Vec{Bool} = Rnd.shuffle([true, false, false])
+    return [Door(car, chosen, false)
+            for (car, chosen) in zip(cars, chosen)]
+end
+
+function openFirstEmptyNonChosenDoor!(doors::Vec{Door})
+    for d in doors
+        if !d.isCar && !d.isChosen
+            d.isOpen = true
+            break
+        end
+    end
+    return nothing
+end
+
+function swapChoice!(doors::Vec{Door})
+    for d in doors
+        if d.isChosen
+            d.isChosen = false
+            continue
+        end
+        if !d.isChosen && !d.isOpen
+            d.isChosen = true
+        end
+    end
+    return nothing
+end
+
+function didTraderWin(doors::Vec{Door})::Bool
+    for d in doors
+        if d.isChosen && d.isCar
+            return true
+        end
+    end
+    return false
+end
