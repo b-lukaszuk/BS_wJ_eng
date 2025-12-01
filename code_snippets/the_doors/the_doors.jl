bd0319ae11112e9ab69dfd191c1a86fa7b124e96
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2025-12-01 17:56:20 +0100

    added part of solution to the_doors.jl

--- code_snippets/the_doors/the_doors.jl
+++ code_snippets/the_doors/the_doors.jl
@@ -38,14 +38,17 @@ mutable struct Door
     Door(car, chosen, open) = new(car, chosen, open)
 end
 
-function get3RandDoors()::Vec{Door}
-    cars::Vec{Bool} = Rnd.shuffle([true, false, false])
-    chosen::Vec{Bool} = Rnd.shuffle([true, false, false])
-    return [Door(car, chosen, false)
-            for (car, chosen) in zip(cars, chosen)]
+function getNRandDoors(n::Int=3)::Vec{Door}
+    @assert 2 < n < 11 "n must be in range [3-10]"
+    cars::Vec{Bool} = zeros(Bool, n)
+    cars[Rnd.rand(1:n)] = true
+    chosen::Vec{Bool} = zeros(Bool, n)
+    chosen[Rnd.rand(1:n)] = true
+    return [Door(car, chosen, false) for (car, chosen) in zip(cars, chosen)]
 end
 
-function openFirstEmptyNonChosenDoor!(doors::Vec{Door})::Vec{Door}
+# open first non-chosen, non-car doors
+function openNonChosenDoor!(doors::Vec{Door})::Vec{Door}
     for d in doors
         if !d.isCar && !d.isChosen
             d.isOpen = true
@@ -55,14 +58,22 @@ function openFirstEmptyNonChosenDoor!(doors::Vec{Door})::Vec{Door}
     return doors
 end
 
+# swap to first non-chosen, non-open door
 function swapChoice!(doors::Vec{Door})::Vec{Door}
+    alreadySwapped::Bool = false
+    alreadyUnselected::Bool = false
     for d in doors
-        if d.isChosen
+        if !alreadyUnselected && d.isChosen
             d.isChosen = false
+            alreadyUnselected = true
             continue
         end
-        if !d.isChosen && !d.isOpen
+        if !alreadySwapped && !d.isChosen && !d.isOpen
             d.isChosen = true
+            alreadySwapped = true
+        end
+        if alreadySwapped && alreadyUnselected
+            break
         end
     end
     return doors
@@ -83,27 +94,28 @@ end
 
 # Solution 2
 # computer simulation
-function getResultOfDoorsGame(shouldSwap::Bool=false)::Bool
-    doors::Vec{Door} = get3RandDoors()
-    openFirstEmptyNonChosenDoor!(doors)
+# single doors game
+function getResultOfDoorsGame(shouldSwap::Bool=false, nDoors::Int=3)::Bool
+    doors::Vec{Door} = getNRandDoors(nDoors)
+    openNonChosenDoor!(doors)
     if shouldSwap
         swapChoice!(doors)
     end
     return didTraderWin(doors)
 end
 
-function getProbOfWinningDoorsGame(shouldSwap::Bool=false,
-                                   nSimul::Int = 10_000,)::Flt
-    return [getResultOfDoorsGame(shouldSwap) for _ in 1:nSimul] |> getAvg
+function getProbOfWinningDoorsGame(nDoors::Int=3, shouldSwap::Bool=false,
+                                   nSimul::Int = 10_000)::Flt
+    return [getResultOfDoorsGame(shouldSwap, nDoors) for _ in 1:nSimul] |> getAvg
 end
 
 Rnd.seed!(1492)
-getProbOfWinningDoorsGame(false), getProbOfWinningDoorsGame(true)
+getProbOfWinningDoorsGame(3, false), getProbOfWinningDoorsGame(3, true)
 
 # Solution 3
 # list all the possibilities, calculate the probabilities
 function getAllDoorSets(doorsPerSet::Int=3)::Vec{Vec{Door}}
-    @assert 1 < doorsPerSet < 11 "doorsPerSet must in the range [1-10]"
+    @assert 2 < doorsPerSet < 11 "doorsPerSet must in the range [3-10]"
     allDoorSets::Vec{Vec{Door}} = []
     subset::Vec{Door} = Door[]
     for i in 1:doorsPerSet, j in 1:doorsPerSet
@@ -116,5 +128,23 @@ function getAllDoorSets(doorsPerSet::Int=3)::Vec{Vec{Door}}
 end
 
 # to get ∘ (function composition) type \circ and press Tab key
-map(didTraderWin, getAllDoorSets()) |> getAvg
-map(didTraderWin ∘ swapChoice!, getAllDoorSets()) |> getAvg
+map(didTraderWin ∘ openNonChosenDoor!, getAllDoorSets()) |> getAvg
+map(didTraderWin ∘ swapChoice! ∘ openNonChosenDoor!, getAllDoorSets()) |> getAvg
+
+# answers for 5 doors per set
+# with computer simulation
+Rnd.seed!(1492)
+getProbOfWinningDoorsGame(5, false), getProbOfWinningDoorsGame(5, true)
+
+# listing all possibiilties
+map(didTraderWin ∘ openNonChosenDoor!, getAllDoorSets(5)) |> getAvg
+map(didTraderWin ∘ swapChoice! ∘ openNonChosenDoor!, getAllDoorSets(5)) |> getAvg
+
+# answers for 7 doors per set
+# with computer simulation
+Rnd.seed!(1492)
+getProbOfWinningDoorsGame(7, false), getProbOfWinningDoorsGame(7, true)
+
+# listing all possibiilties
+map(didTraderWin ∘ openNonChosenDoor!, getAllDoorSets(7)) |> getAvg
+map(didTraderWin ∘ swapChoice! ∘ openNonChosenDoor!, getAllDoorSets(7)) |> getAvg
