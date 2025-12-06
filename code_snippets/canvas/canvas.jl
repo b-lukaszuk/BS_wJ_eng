5b6521b10edef660bd5b2088f26644e10570fe71
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2025-12-06 21:20:50 +0100

    added part of solution to canvas.jl

--- code_snippets/canvas/canvas.jl
+++ code_snippets/canvas/canvas.jl
@@ -3,6 +3,7 @@
 
 # TODO
 const Pt = Tuple{Int, Int} # (x, y)
+const Vec = Vector
 
 canvas = fill(' ', 40, 40) # top-left corner (1, 1)
 
@@ -26,4 +27,18 @@ end
 
 function isSamePt(pt1::Pt, pt2::Pt)::Bool
     return pt1 .- pt2 == (0, 0)
+end
+
+function getHorizontalLine(pt1::Pt, pt2::Pt)::Vec{Pt}
+    x1, y1 = pt1
+    x2, y2 = pt2
+    @assert y1 == y2 "the ys are not equal"
+    return [(i, y1) for i in x1:x2]
+end
+
+function getVerticalLine(pt1::Pt, pt2::Pt)::Vec{Pt}
+    x1, y1 = pt1
+    x2, y2 = pt2
+    @assert x1 == x2 "the xs are not equal"
+    return [(x1, i) for i in y1:y2]
 end
