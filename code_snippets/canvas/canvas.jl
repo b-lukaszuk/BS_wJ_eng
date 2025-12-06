88535dc2a6fc16356941586f76cf848157ed80ec
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2025-12-06 21:11:55 +0100

    added part of solution to canvas.jl

--- code_snippets/canvas/canvas.jl
+++ code_snippets/canvas/canvas.jl
@@ -2,3 +2,28 @@
 # it may not act correctly
 
 # TODO
+const Pt = Tuple{Int, Int} # (x, y)
+
+canvas = fill(' ', 40, 40) # top-left corner (1, 1)
+
+function printCanvas(cvs::Matrix{Char})
+    nRows, _ = size(cvs)
+    for r in 1:nRows
+        println(cvs[r, :] |> join)
+    end
+    return nothing
+end
+
+function isOutsideGrid(pt::Pt, cvs::Matrix{Char}=canvas)::Bool
+    nRows, nCols = size(cvs)
+    x, y = pt
+    return (x > nCols || x < 1) || (y > nRows || y < 1)
+end
+
+function isWithinGrid(pt::Pt, cvs::Matrix{Char}=canvas)::Bool
+    return !isOutsideGrid(pt, cvs)
+end
+
+function isSamePt(pt1::Pt, pt2::Pt)::Bool
+    return pt1 .- pt2 == (0, 0)
+end
