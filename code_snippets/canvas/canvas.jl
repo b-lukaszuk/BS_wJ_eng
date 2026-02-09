9b7de38af753b50b9c2f12f8e1284df71922553e
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2026-02-09 20:40:11 +0100

    cosmetic changes in canvas.jl

--- code_snippets/canvas/canvas.jl
+++ code_snippets/canvas/canvas.jl
@@ -13,6 +13,10 @@ function getGrayBg(s::Str)::Str
     return "\x1b[100m" * s * "\x1b[0m"
 end
 
+function getWhiteBg(s::Str)::Str
+    return "\x1b[107m" * s * "\x1b[0m"
+end
+
 canvas = fill(getGrayBg(" "), 20, 40) # top-left corner (1, 1)
 
 function printCanvas(cvs::Matrix{Str}=canvas)::Nothing
@@ -28,15 +32,13 @@ function clearCanvas!(cvs::Matrix{Str}=canvas)::Nothing
     return nothing
 end
 
-printCanvas()
-
-function isOutsideGrid(pt::Pt, cvs::Matrix{Char}=canvas)::Bool
+function isOutsideGrid(pt::Pt, cvs::Matrix{Str}=canvas)::Bool
     nRows, nCols = size(cvs)
     x, y = pt
     return (x > nCols || x < 1) || (y > nRows || y < 1)
 end
 
-function isWithinGrid(pt::Pt, cvs::Matrix{Char}=canvas)::Bool
+function isWithinGrid(pt::Pt, cvs::Matrix{Str}=canvas)::Bool
     return !isOutsideGrid(pt, cvs)
 end
 
@@ -44,37 +46,28 @@ function isSamePt(pt1::Pt, pt2::Pt)::Bool
     return pt1 .- pt2 == (0, 0)
 end
 
-function getHorizontalLine(pt1::Pt, pt2::Pt)::Vec{Pt}
-    x1, y1 = pt1
-    x2, y2 = pt2
-    @assert y1 == y2 "the ys are not equal"
-    return [(i, y1) for i in x1:x2]
-end
-
-function getVerticalLine(pt1::Pt, pt2::Pt)::Vec{Pt}
-    x1, y1 = pt1
-    x2, y2 = pt2
-    @assert x1 == x2 "the xs are not equal"
-    return [(x1, i) for i in y1:y2]
-end
-
-function addPoints!(line::Vec{Pt}, cvs::Matrix{Char}=canvas)
+function addPoints!(line::Vec{Pt}, cvs::Matrix{Str}=canvas)::Nothing
     for pt in line
-        col, row = pt
-        cvs[row, col] = '*'
+        if isWithinGrid(pt)
+            cvs[pt...] = getWhiteBg(" ")
+        end
     end
     return nothing
 end
 
-function getRectangle(topLeft::Pt, width::Int, height::Int)::Vec{Pt}
+function getRectangle(width::Int, height::Int)::Vec{Pt}
     @assert width >= 2 "width must be >= 2"
     @assert height >= 2 "height must be >= 2"
-    x::Int, y::Int = topLeft
-    rectangle::Vec{Pt} = [
-        getHorizontalLine(topLeft, (x+width, y))...,
-        getVerticalLine(topLeft, (x, y+height))...,
-        getHorizontalLine((x, y+height), (x+width, y+height) )...,
-        getVerticalLine((x+width, y), (x+width, y+height))...
-    ]
+    nRects::Int = width * height
+    rectangle::Vec{Pt} = Vec{Pt}(undef, nRects)
+    i::Int = 1
+    for row in 1:height, col in 1:width
+        rectangle[i] = (row, col)
+        i += 1
+    end
     return rectangle
 end
+
+clearCanvas!()
+addPoints!(getRectangle(6, 3))
+printCanvas()
