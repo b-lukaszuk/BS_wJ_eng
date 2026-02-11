c0997de13390305d06eb28de43cb39212ba1bdc6
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2026-02-11 21:30:07 +0100

    cosmetic changes in canvas.jl

--- code_snippets/canvas/canvas.jl
+++ code_snippets/canvas/canvas.jl
@@ -6,25 +6,24 @@ const Location = Tuple{Int, Int} # (row, col) in canvas
 const Vec = Vector
 const Str = String
 
-const RATIO = 2 # a character in a font is 2 units high, 1 unit wide
 const PIXEL = " " # empty string, because we set background color
 
 # https://en.wikipedia.org/wiki/ANSI_escape_code#Colors
-# "\x1b[XXXm" sets background color to XXX color code
+# "\x1b[48:5:XXXm" sets background color to XXX color code (256-color mode)
 const BG_COLORS = Dict(
-    :gray => "\x1b[100m",
-    :white => "\x1b[107m",
-    :red => "\x1b[41m",
-    :yellow => "\x1b[43m",
-    :blue => "\x1b[44m"
+    :gray => "\x1b[48:5:8m",
+    :white => "\x1b[48:5:15m",
+    :red => "\x1b[48:5:160m",
+    :yellow => "\x1b[48:5:11m",
+    :blue => "\x1b[48:5:12m",
+    :green => "\x1b[48:5:35m",
+    :black => "\x1b[48:5:0m",
 )
 
 # "\x1b[0m" resets background color to default value
 function getBgColor(color::Symbol, colors::Dict{Symbol, Str}=BG_COLORS)::Str
-    return get(colors, color, :gray) * PIXEL * "\x1b[0m"
+    return get(colors, color, "\x1b[48:5:8m") * PIXEL * "\x1b[0m"
 end
-
-canvas = fill(getBgColor(:gray), 30, 30*RATIO) # top-left corner (1, 1)
 
 function printCanvas(cvs::Matrix{Str}=canvas)::Nothing
     nRows, _ = size(cvs)
@@ -39,19 +38,15 @@ function clearCanvas!(cvs::Matrix{Str}=canvas)::Nothing
     return nothing
 end
 
-function isWithinGrid(point::Location, cvs::Matrix{Str}=canvas)::Bool
+function isWithinCanvas(point::Location, cvs::Matrix{Str}=canvas)::Bool
     nRows, nCols = size(cvs)
     row, col = point
-    return (0 < row <= nRows) || (0 < col <= nCols)
-end
-
-function isSamePt(pt1::Location, pt2::Location)::Bool
-    return pt1 .- pt2 == (0, 0)
+    return (0 < row <= nRows) && (0 < col <= nCols)
 end
 
 function addPoints!(line::Vec{Location}, color::Symbol, cvs::Matrix{Str}=canvas)::Nothing
     for pt in line
-        if isWithinGrid(pt)
+        if isWithinCanvas(pt)
             cvs[pt...] = getBgColor(color)
         end
     end
@@ -61,7 +56,6 @@ end
 function getRectangle(width::Int, height::Int)::Vec{Location}
     @assert width >= 2 "width must be >= 2"
     @assert height >= 2 "height must be >= 2"
-    width *= RATIO
     nRects::Int = width * height
     rectangle::Vec{Location} = Vec{Location}(undef, nRects)
     i::Int = 1
@@ -74,7 +68,7 @@ end
 
 function getTriangle(height::Int)::Vec{Location}
     @assert height > 1 "height must be > 1"
-    startCol::Int = 10
+    startCol::Int = 1
     lCol::Int = startCol
     rCol::Int = startCol
     triangle::Vec{Location} = []
@@ -90,7 +84,7 @@ end
 
 function getCircle(radius::Int)::Vec{Location}
     @assert radius > 1 "radius must be > 1"
-    cols::Vec{Vec{Int}} = [collect((9-r):(10+r)) for r in 0:(radius-1)]
+    cols::Vec{Vec{Int}} = [collect((0-r):(1+r)) for r in 0:(radius-1)]
     cols = [cols..., reverse(cols)...]
     triangle::Vec{Location} = []
     for row in 1:(radius*2)
@@ -105,8 +99,14 @@ function nudge(shape::Vec{Location}, by::Location)::Vec{Location}
     return map(pt -> pt .+ by, shape)
 end
 
+canvas = fill(getBgColor(:gray), 30, 60) # top-left corner (1, 1)
+
 clearCanvas!()
-addPoints!(nudge(getRectangle(16, 8), (10, 0)), :white)
-addPoints!(getTriangle(10), :red)
-addPoints!(nudge(getCircle(3), (0, 10)), :yellow)
+addPoints!(nudge(getRectangle(60, 15), (15, 0)), :green)
+addPoints!(getRectangle(60, 15), :blue)
+addPoints!(getRectangle(60, 15), :blue)
+addPoints!(nudge(getRectangle(15, 8), (15, 20)), :white)
+addPoints!(nudge(getRectangle(4, 6), (8, 30)), :black)
+addPoints!(nudge(getTriangle(8), (7, 27)), :red)
+addPoints!(nudge(getCircle(3), (2, 50)), :yellow)
 printCanvas()
