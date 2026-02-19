337df08c22d386d4579ce38c583e38d4f18fb92f
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2026-02-19 20:53:46 +0100

    cosmetic changes in the_doors.jl

--- code_snippets/the_doors/the_doors.jl
+++ code_snippets/the_doors/the_doors.jl
@@ -12,10 +12,10 @@ const Vec = Vector
 #                                  solution 1                                 #
 ###############################################################################
 df = Dfs.DataFrame(Dict("Door" => 1:3))
-df.prior = repeat([1//3], 3) # 1//3 is Rational (a fraction)
+df.prior = repeat([1/3], 3)
 df
 
-df.likelihood = [1//2, 1, 0]
+df.likelihood = [1/2, 1, 0]
 df
 
 function bayesUpdate!(df::Dfs.DataFrame)::Nothing
@@ -25,7 +25,7 @@ function bayesUpdate!(df::Dfs.DataFrame)::Nothing
 end
 
 bayesUpdate!(df)
-df # to see Rationals as floats: convert.(Flt, df.posterior)
+df
 
 ###############################################################################
 #                                  solution 2                                 #
@@ -36,26 +36,31 @@ mutable struct Door
     isOpen::Bool
 end
 
-function get3RandDoors()::Vec{Door}
-    cars::Vec{Bool} = Rnd.shuffle([true, false, false])
-    chosen::Vec{Bool} = Rnd.shuffle([true, false, false])
-    return [Door(car, chosen, false)
-            for (car, chosen) in zip(cars, chosen)]
+
+# get n random doors, 1 with a car, 1 chosen by the trader
+function getNRandDoors(n::Int)::Vec{Door}
+    @assert 2 < n < 10 "n must be in range [3-9]"
+    doors::Vec{Door} = [Door(false, false, false) for _ in 1:n]
+    doors[Rnd.rand(1:n)].isCar = true
+    doors[Rnd.rand(1:n)].isChosen = true
+    return doors
 end
 
-# open first non-chosen, non-car door
+# open random non-chosen, non-car door
 function openEligibleDoor!(doors::Vec{Door})::Nothing
-    @assert length(doors) == 3 "need 3 doors"
-    indEligible::Int = findfirst(d -> !d.isCar && !d.isChosen, doors)
-    doors[indEligible].isOpen = true
+    @assert 2 < length(doors) < 10 "length(doors) must be in range [3-9]"
+    indsEligible::Vec{Int} = findall(d -> !d.isCar && !d.isChosen, doors)
+    ind2Open::Int = indsEligible[Rnd.rand(1:length(indsEligible))]
+    doors[ind2Open].isOpen = true
     return nothing
 end
 
-# swap to first non-chosen, non-open door
+# swap to random non-chosen, non-open door
 function swapChoice!(doors::Vec{Door})::Nothing
-    @assert length(doors) == 3 "need 3 doors"
+    @assert 2 < length(doors) < 10 "length(doors) must be in range [3-9]"
     indCurChosen::Int = findfirst(d -> d.isChosen, doors)
-    ind2Choose::Int = findfirst(d -> !d.isChosen && !d.isOpen, doors)
+    inds2Choose::Vec{Int} = findall(d -> !d.isChosen && !d.isOpen, doors)
+    ind2Choose::Int = inds2Choose[Rnd.rand(1:length(inds2Choose))]
     doors[indCurChosen].isChosen = false
     doors[ind2Choose].isChosen = true
     return nothing
@@ -67,8 +72,8 @@ function didTraderWin(doors::Vec{Door})::Bool
     return isnothing(indWinner) ? false : true
 end
 
-function getResultOfDoorsGame(shouldSwap::Bool=false)::Bool
-    doors::Vec{Door} = get3RandDoors()
+function getResultOfDoorsGame(nDooors::Int, shouldSwap::Bool=false)::Bool
+    doors::Vec{Door} = getNRandDoors(nDooors)
     openEligibleDoor!(doors)
     if shouldSwap
         swapChoice!(doors)
@@ -81,23 +86,25 @@ function getAvg(successes::Vec{Bool})::Flt
     return sum(successes) / length(successes)
 end
 
-function getProbOfWinningDoorsGame(shouldSwap::Bool=false,
+function getProbOfWinningDoorsGame(nDoors::Int, shouldSwap::Bool=false,
                                    nSimul::Int=10_000)::Flt
     @assert 1e3 <= nSimul <= 1e5 "nSimul must be in range [1e3 - 1e5]"
-    return [getResultOfDoorsGame(shouldSwap) for _ in 1:nSimul] |> getAvg
+    return [getResultOfDoorsGame(nDoors, shouldSwap) for _ in 1:nSimul] |> getAvg
 end
 
 Rnd.seed!(1492)
-getProbOfWinningDoorsGame(false), getProbOfWinningDoorsGame(true)
+getProbOfWinningDoorsGame(3, false)
+getProbOfWinningDoorsGame(3, true)
 
 ###############################################################################
 #                                  solution 3                                 #
 ###############################################################################
-function getAll3DoorSets()::Vec{Vec{Door}}
+function getAllDoorSets(nDoorsPerSet::Int)::Vec{Vec{Door}}
+    @assert 2 < nDoorsPerSet < 10 "nDoorsPerSet must be in range [3-9]"
     allDoorSets::Vec{Vec{Door}} = []
     subset::Vec{Door} = Door[]
-    for indCar in 1:3, indChosen in 1:3
-        subset = [Door(false, false, false) for _ in 1:3]
+    for indCar in 1:nDoorsPerSet, indChosen in 1:nDoorsPerSet
+        subset = [Door(false, false, false) for _ in 1:nDoorsPerSet]
         subset[indCar].isCar = true
         subset[indChosen].isChosen = true
         push!(allDoorSets, subset)
@@ -105,24 +112,26 @@ function getAll3DoorSets()::Vec{Vec{Door}}
     return allDoorSets
 end
 
-# open first non-chosen, non-car door
+# open random non-chosen, non-car door
 function openEligibleDoor!(doors::Vec{Door})::Vec{Door}
-    @assert length(doors) == 3 "need 3 doors"
-    indEligible::Int = findfirst(d -> !d.isCar && !d.isChosen, doors)
-    doors[indEligible].isOpen = true
-    return doors # instead of: return nothing
+    @assert 2 < length(doors) < 10 "length(doors) must be in range [3-9]"
+    indsEligible::Vec{Int} = findall(d -> !d.isCar && !d.isChosen, doors)
+    ind2Open::Int = indsEligible[Rnd.rand(1:length(indsEligible))]
+    doors[ind2Open].isOpen = true
+    return doors
 end
 
-# swap to first non-chosen, non-open door
+# swap to random non-chosen, non-open door
 function swapChoice!(doors::Vec{Door})::Vec{Door}
-    @assert length(doors) == 3 "need 3 doors"
+    @assert 2 < length(doors) < 10 "length(doors) must be in range [3-9]"
     indCurChosen::Int = findfirst(d -> d.isChosen, doors)
-    ind2Choose::Int = findfirst(d -> !d.isChosen && !d.isOpen, doors)
+    inds2Choose::Vec{Int} = findall(d -> !d.isChosen && !d.isOpen, doors)
+    ind2Choose::Int = inds2Choose[Rnd.rand(1:length(inds2Choose))]
     doors[indCurChosen].isChosen = false
     doors[ind2Choose].isChosen = true
-    return doors # instead of: return nothing
+    return doors
 end
 
-map(didTraderWin ∘ openEligibleDoor!, getAll3DoorSets()) |> getAvg,
+map(didTraderWin ∘ openEligibleDoor!, getAllDoorSets(3)) |> getAvg,
 map(didTraderWin ∘ swapChoice! ∘ openEligibleDoor!,
-    getAll3DoorSets()) |> getAvg
+    getAllDoorSets(3)) |> getAvg
