99ae89536e31020a1106a328a15503f9d031175d
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2026-03-10 17:03:04 +0100

    added part of solution to sort.jl

--- code_snippets/sort/sort.jl
+++ code_snippets/sort/sort.jl
@@ -22,3 +22,20 @@ end
 
 xs = [47, 15, 23, 99, 4]
 sort(xs) == qs(xs)
+
+# by - (A) -> B is applied to every elt of v before sorting
+# lt - (B) -> Bool is applied to every elt of v after by was applied
+function qs(v::Vec{A}, by::Function, lt::Function)::Vec{A} where A
+    if isempty(v)
+        return []
+    else
+        ind::Int = 1
+        pivotElt::A = v[ind]
+        restV::Vec{A} = v[ind+1:end]
+        smallerElts::Vec{A} = filter(elt -> lt(by(elt), by(pivotElt)), restV)
+        greaterEqElts::Vec{A} = filter(elt -> !lt(by(elt), by(pivotElt)), restV)
+        return [qs(smallerElts, by, lt); pivotElt; qs(greaterEqElts, by, lt)]
+    end
+end
+
+qs(xs, identity, <) == sort(xs, by=identity, lt=<)
