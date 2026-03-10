ef365aa589e709fd02657b9bdee4474b8f287fee
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2026-03-10 16:56:53 +0100

    added part of solution to sort.jl

--- code_snippets/sort/sort.jl
+++ code_snippets/sort/sort.jl
@@ -1,4 +1,24 @@
+# TODO: sort
+# https://en.wikipedia.org/wiki/Sorting_algorithm
+
+const Vec = Vector
+
 # the code in this file is meant to serve as a programming exercise only
 # it may not act correctly
 
-# TODO: sort
+# https://en.wikipedia.org/wiki/Quicksort
+function qs(v::Vec{Int})::Vec{Int}
+    if isempty(v)
+        return []
+    else
+        ind::Int = 1
+        pivotElt::Int = v[ind]
+        restV::Vec{Int} = v[ind+1:end]
+        smallerElts::Vec{Int} = filter(elt -> elt < pivotElt, restV)
+        greaterEqElts::Vec{Int} = filter(elt -> elt >= pivotElt, restV)
+        return [qs(smallerElts); pivotElt; qs(greaterEqElts)]
+    end
+end
+
+xs = [47, 15, 23, 99, 4]
+sort(xs) == qs(xs)
