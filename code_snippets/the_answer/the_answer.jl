a0cff459673ce05b0ae013b61e47c9e85f43fffc
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2026-03-24 19:24:33 +0100

    cosmetic changes in the_answer.jl

--- code_snippets/the_answer/the_answer.jl
+++ code_snippets/the_answer/the_answer.jl
@@ -20,12 +20,16 @@ function isBaseN(num::Str, n::Int)::Bool
     return isBaseN.(collect(num), n) |> all
 end
 
+# toInd0 and toInd1 assume correct inputs/outputs w/o checks
+toIndBased0(oneBasedInd::Int)::Int = oneBasedInd - 1
+toIndBased1(zeroBasedInd::Int)::Int = zeroBasedInd + 1
+
 function dec2baseN(dec::Int, n::Int)::Str
     @assert MIN_BASE <= n <= MAX_BASE "n not in [$MIN_BASE - $MAX_BASE]"
     @assert 0 <= dec <= 1024 "dec must be in range [0-1024]"
     result::Str = ""
     while dec != 0
-        result *= CHARS[(dec % n) + 1] # 0 based to 1 based
+        result *= CHARS[(dec % n) |> toIndBased1]
         dec = div(dec, n)
     end
     return isempty(result) ? "0" : reverse(result)
@@ -35,43 +39,44 @@ end
 [dec2baseN(i, b) == string(i, base=b)
  for b in 2:16 for i in 0:1024] |> all
 
-# returns (carried num, running num)
-function add(x::Char, y::Char, base::Int)::Tuple{Char, Char}
-    @assert isBaseN(x, base) "$x is not a num of base $base"
-    @assert isBaseN(y, base) "$y is not a num of base $base"
-    @assert MIN_BASE <= base <= MAX_BASE "$base not in [$MIN_BASE - $MAX_BASE]"
-    ind1::Int = findfirst(x, CHARS |> join) - 1 # 0 based
-    ind2::Int = findfirst(y, CHARS |> join) - 1 # 0 based
-    sumInd::Int = ind1 + ind2 # 0 based
-    if sumInd >= base
+# returns (crried num, running num)
+function add(num1::Char, num2::Char, numsBase::Int)::Tuple{Char, Char}
+    @assert isBaseN(num1, numsBase) "$num1 is not a num of numsBase $numsBase"
+    @assert isBaseN(num2, numsBase) "$num2 is not a num of numsBase $numsBase"
+    @assert MIN_BASE <= numsBase <= MAX_BASE "$numsBase not in [$MIN_BASE - $MAX_BASE]"
+    base0ind1::Int = findfirst(num1, join(CHARS)) |> toIndBased0
+    base0ind2::Int = findfirst(num2, join(CHARS)) |> toIndBased0
+    base0sumInds::Int = base0ind1 + base0ind2
+    if base0sumInds >= numsBase
         carry::Char = '1'
-        runningInd::Int = (sumInd % base) + 1 # 1 based
-        return (carry, CHARS[runningInd])
+        base0runningInd::Int = (base0sumInds % numsBase)
+        return (carry, CHARS[base0runningInd |> toIndBased1])
     else
-        return ('0', CHARS[sumInd+1]) # 1 based
+        return ('0', CHARS[base0sumInds |> toIndBased1])
     end
 end
 
 # tests
 # with printout
-for b in 2:16
-    println("\n---base = $b---")
-    for x in 0:(b-1), y in 0:(b-1)
-        n1 = dec2baseN(x, b)[1]
-        n2 = dec2baseN(y, b)[1]
-        println("add($n1, $n2, base=$b) = $(add(n1, n2, b)) = $(dec2baseN(x+y, b))")
+for base in 2:16
+    println("\n---base = $base---")
+    for dec1 in 0:(base-1), dec2 in 0:(base-1)
+        n1 = dec2baseN(dec1, base)[1]
+        n2 = dec2baseN(dec2, base)[1]
+        println("add($n1, $n2, base=$base) = ",
+                "$(add(n1, n2, base)) = $(dec2baseN(dec1+dec2, base))")
     end
 end
 
 # without printout
 tests = Bool[]
-for b in 2:16 # base
-    for x in 0:(b-1), y in 0:(b-1) # nums
-        n1 = dec2baseN(x, b)[1]
-        n2 = dec2baseN(y, b)[1]
-        result = join(add(n1, n2, b))
+for base in 2:16
+    for dec1 in 0:(base-1), dec2 in 0:(base-1) # dec - decimal
+        num1 = dec2baseN(dec1, base)[1]
+        num2 = dec2baseN(dec2, base)[1]
+        result = join(add(num1, num2, base))
         result = result == "00" ? "0" : lstrip(result, '0')
-        push!(tests, result == dec2baseN(x+y, b))
+        push!(tests, result == dec2baseN(dec1+dec2, base))
     end
 end
 all(tests)
