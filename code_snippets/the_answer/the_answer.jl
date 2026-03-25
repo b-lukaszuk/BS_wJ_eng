dec150203c3f46c7a0ccc9ad0ffe220e78a2e531
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2026-03-25 21:46:25 +0100

    cosmetic changes in the_answer.jl

--- code_snippets/the_answer/the_answer.jl
+++ code_snippets/the_answer/the_answer.jl
@@ -5,7 +5,6 @@
 # it may not act correctly
 
 const Str = String
-const Vec = Vector
 
 const CHARS = "0123456789abcdef" |> collect
 const MIN_BASE = 2
@@ -20,16 +19,12 @@ function isBaseN(num::Str, n::Int)::Bool
     return isBaseN.(collect(num), n) |> all
 end
 
-# toInd0 and toInd1 assume correct inputs/outputs w/o checks
-toIndBased0(oneBasedInd::Int)::Int = oneBasedInd - 1
-toIndBased1(zeroBasedInd::Int)::Int = zeroBasedInd + 1
-
 function dec2baseN(dec::Int, n::Int)::Str
     @assert MIN_BASE <= n <= MAX_BASE "n not in [$MIN_BASE - $MAX_BASE]"
     @assert 0 <= dec <= 1024 "dec must be in range [0-1024]"
     result::Str = ""
     while dec != 0
-        result *= CHARS[(dec % n) |> toIndBased1]
+        result *= CHARS[(dec % n) + 1] # d ec % n - 0 based C-like indexing
         dec = div(dec, n)
     end
     return isempty(result) ? "0" : reverse(result)
@@ -37,100 +32,9 @@ end
 
 # test against Julia's built-it  functionality
 [dec2baseN(i, b) == string(i, base=b)
- for b in 2:16 for i in 0:1024] |> all
+ for b in MIN_BASE:MAX_BASE for i in 0:1024] |> all
 
-# returns (crried num, running num)
-function add(num1::Char, num2::Char, numsBase::Int)::Tuple{Char, Char}
-    @assert isBaseN(num1, numsBase) "$num1 is not a num of numsBase $numsBase"
-    @assert isBaseN(num2, numsBase) "$num2 is not a num of numsBase $numsBase"
-    @assert MIN_BASE <= numsBase <= MAX_BASE "$numsBase not in [$MIN_BASE - $MAX_BASE]"
-    base0ind1::Int = findfirst(num1, join(CHARS)) |> toIndBased0
-    base0ind2::Int = findfirst(num2, join(CHARS)) |> toIndBased0
-    base0sumInds::Int = base0ind1 + base0ind2
-    if base0sumInds >= numsBase
-        carry::Char = '1'
-        base0runningInd::Int = (base0sumInds % numsBase)
-        return (carry, CHARS[base0runningInd |> toIndBased1])
-    else
-        return ('0', CHARS[base0sumInds |> toIndBased1])
-    end
-end
-
-# tests
-# with printout
-for base in 2:16
-    println("\n---base = $base---")
-    for dec1 in 0:(base-1), dec2 in 0:(base-1)
-        n1 = dec2baseN(dec1, base)[1]
-        n2 = dec2baseN(dec2, base)[1]
-        println("add($n1, $n2, base=$base) = ",
-                "$(add(n1, n2, base)) = $(dec2baseN(dec1+dec2, base))")
-    end
-end
-
-# without printout
-tests = Bool[]
-for base in 2:16
-    for dec1 in 0:(base-1), dec2 in 0:(base-1) # dec - decimal
-        num1 = dec2baseN(dec1, base)[1]
-        num2 = dec2baseN(dec2, base)[1]
-        result = join(add(num1, num2, base))
-        result = result == "00" ? "0" : lstrip(result, '0')
-        push!(tests, result == dec2baseN(dec1+dec2, base))
-    end
-end
-all(tests)
-
-function getEqlLenNums(num1::Str, num2::Str)::Tuple{Str, Str}
-    if length(num1) >= length(num2)
-        return (num1, lpad(num2, length(num1), '0'))
-    else
-        return getEqlLenNums(num2, num1)
-    end
-end
-
-function isZero(num::Char)::Bool
-    return num == '0'
-end
-
-function isZero(num::Str)::Bool
-    return isZero.(collect(num)) |> all
-end
-
-function add(num1::Str, num2::Str, baseNums::Int)::Str
-    num1, num2 = getEqlLenNums(num1, num2)
-    carriedSlot::Char = '0'
-    runningSlot::Char = '0'
-    runningSlots::Str = ""
-    carriedSlots::Str = "0"
-    for (n1, n2) in zip(reverse(num1), reverse(num2))
-        carriedSlot, runningSlot = add(n1, n2, baseNums)
-        runningSlots = runningSlot * runningSlots
-        carriedSlots = carriedSlot * carriedSlots
-    end
-    if isZero(carriedSlots)
-        return isZero(runningSlots) ? "0" : lstrip(isZero, runningSlots)
-    else
-        return add(runningSlots, carriedSlots, baseNums)
-    end
-end
-
-# baseNFn(Str, Str, Int) -> Str, decFn(Int, Int) -> Int
-function doesBaseNFnWork(dec1::Int, dec2::Int,
-                       baseNFn::Function, baseN::Int,
-                       decFn::Function)::Bool
-    num1::Str = baseNFn(dec2baseN(dec1, baseN), dec2baseN(dec2, baseN), baseN)
-    num2::Str = string(decFn(dec1, dec2), base=baseN)
-    return num1 == num2
-end
-
-# running this test may take a few seconds (513x513 matrix)
-for base in 2:16
-    tests = [doesBaseNFnWork(a, b, add, 2, +) for a in 0:100, b in 0:100];
-    println("base $base, tests passed? ", all(tests))
-end
-
-for base in 2:15
+for base in MIN_BASE:MAX_BASE
     for dec1 in 0:(base-1), dec2 in 0:(base-1)
         n1 = dec2baseN(dec1, base)
         n2 = dec2baseN(dec2, base)
