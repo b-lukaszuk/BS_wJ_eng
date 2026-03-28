c16099102521e0030d6fd784949212cdda9a7c7e
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2026-03-28 20:26:58 +0100

    cosmetic changes in the_answer.jl

--- code_snippets/the_answer/the_answer.jl
+++ code_snippets/the_answer/the_answer.jl
@@ -1,12 +1,9 @@
-# TODO the_answer
-# https://en.wikipedia.org/wiki/Phrases_from_The_Hitchhiker%27s_Guide_to_the_Galaxy#The_Answer_to_the_Ultimate_Question_of_Life,_the_Universe,_and_Everything_is_42
-
 # the code in this file is meant to serve as a programming exercise only
 # it may not act correctly
 
 const Str = String
 
-const CHARS = "0123456789abcdef" |> collect
+const CHARS = vcat('0':'9', 'a':'f')
 const MIN_BASE = 2
 const MAX_BASE = 16
 
@@ -15,7 +12,8 @@ function dec2baseN(dec::Int, n::Int)::Str
     @assert 0 <= dec <= 1024 "dec must be in range [0-1024]"
     result::Str = ""
     while dec != 0
-        result *= CHARS[(dec % n) + 1] # d ec % n - 0 based C-like indexing
+        # (dec % n) - C-like indexing [0 - (n-1)]
+        result *= CHARS[(dec % n) + 1]
         dec = div(dec, n)
     end
     return isempty(result) ? "0" : reverse(result)
@@ -25,8 +23,9 @@ end
 [dec2baseN(i, b) == string(i, base=b)
  for b in MIN_BASE:MAX_BASE for i in 0:1024] |> all
 
+# which 1-digit nums multiplied by each other give 42
 for base in MIN_BASE:MAX_BASE
-    for dec1 in 0:(base-1), dec2 in 0:(base-1)
+    for dec1 in 0:(base-1), dec2 in 0:(base-1) # 1 digit nums only
         n1 = dec2baseN(dec1, base)
         n2 = dec2baseN(dec2, base)
         product = dec2baseN(dec1 * dec2, base)
