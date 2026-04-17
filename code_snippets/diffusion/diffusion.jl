f5036326ab848c6acf689c3c1102d1ce23a7fb94
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2026-04-17 17:46:24 +0200

    added part of solution to diffusion.jl

--- code_snippets/diffusion/diffusion.jl
+++ code_snippets/diffusion/diffusion.jl
@@ -2,3 +2,28 @@
 # it may not act correctly
 
 # TODO: diffusion
+# https://en.wikipedia.org/wiki/Diffusion
+
+const Pos = Tuple{Int, Int} # position, (row, col) in canvas
+const Vec = Vector
+
+const N_COLS = 80
+const N_ROWS = 40
+
+# marix of chars
+container = fill(' ', N_ROWS, N_COLS);
+
+function printContainer(cvs::Matrix{Char}=container)::Nothing
+    nRows, _ = size(cvs)
+    for r in 1:nRows
+        println(cvs[r, :] |> join)
+    end
+    return nothing
+end
+
+function clearContainer!(container::Matrix{Char}=container)::Nothing
+    container .= ' '
+    return nothing
+end
+
+printContainer()
