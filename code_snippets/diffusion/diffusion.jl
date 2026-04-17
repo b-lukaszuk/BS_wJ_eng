3b1138fec55d6d7d936e4bcb83c52b689fe42575
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2026-04-17 23:30:21 +0200

    added part of solution to diffusion.jl

--- code_snippets/diffusion/diffusion.jl
+++ code_snippets/diffusion/diffusion.jl
@@ -9,14 +9,16 @@
 # where n - dimension, D - diffusion coefficient, t - time
 # perhaps I should remove sqrt(2nD⧋t) to simplify it
 # plus need collision detection
+# (or maybe just let them pass through in 3rd D, like in: https://en.wikipedia.org/wiki/File:Brownian_motion_large.gif
 
+const Flt = Float64
 const Pos = Tuple{Int, Int} # position, (row, col) in 2D container
 const Str = String
 const Vec = Vector
 
 const N_COLS = 80
 const N_ROWS = 40
-const N_MOLECULES = 30
+const N_MOLECULES = 100
 const MOLECULE = '.'
 
 function printContainer(container::Matrix{Char})::Nothing
@@ -24,11 +26,6 @@ function printContainer(container::Matrix{Char})::Nothing
     for r in 1:nRows
         println(container[r, :] |> join)
     end
-    return nothing
-end
-
-function clearContainer!(container::Matrix{Char})::Nothing
-    container .= ' '
     return nothing
 end
 
@@ -40,14 +37,31 @@ function addBorders!(container::Matrix{Char})::Nothing
     return nothing
 end
 
+function isWithinContainer(molecule::Pos, container::Matrix{Char})::Bool
+    nRows, nCols = size(container)
+    row, col = molecule
+    # corrected for borders
+    return (1 < row <= nRows-1) && (1 < col <= nCols-1)
+end
+
+function clearContainer!(container::Matrix{Char})::Nothing
+    for c in 1:N_COLS, r in 1:N_ROWS
+        if isWithinContainer((r, c), container)
+            container[r, c] = ' '
+        end
+    end
+    return nothing
+end
+
 function rndPlaceMolecules!(molecules::Vec{Pos},
+                            rowsMinMax::Tuple{Int, Int},
+                            colsMinMax::Tuple{Int, Int},
                             nMolecules::Int=N_MOLECULES)::Nothing
     i::Int = 1
-    r::Int = 0
-    c::Int = 0
+    r::Int, c::Int = 0, 0
     while i <= nMolecules
-        r = rand(2:N_ROWS-1)
-        c = rand(2:N_COLS-1)
+        r = rand(range(rowsMinMax...))
+        c = rand(range(colsMinMax...))
         if !in((r, c), molecules)
             molecules[i] = (r, c)
             i += 1
@@ -56,17 +70,64 @@ function rndPlaceMolecules!(molecules::Vec{Pos},
     return nothing
 end
 
-function isWithinContainer(molecule::Pos, container::Matrix{Char})::Bool
-    nRows, nCols = size(container)
-    row, col = molecule
-    return (0 < row <= nRows) && (0 < col <= nCols)
-end
-
 function addMolecules!(molecules::Vec{Pos}, container!::Matrix{Char})::Nothing
     for molecule in molecules
         if isWithinContainer(molecule, container!)
             container![molecule...] = MOLECULE
         end
+    end
+    return nothing
+end
+
+roundFlt2int(f::Flt)::Int = round(Int, f)
+
+function moveMolecule(molecule::Pos)::Pos
+    r::Int = roundFlt2int(randn())
+    c::Int = roundFlt2int(randn())
+    return molecule .+ (r, c)
+end
+
+# assumption: molecules may pass through each other (or occupy the same pixel in 2D)
+# since they move past each other in the third (not drawn) dimension
+function moveBrownian!(molecules!::Vec{Pos}, container::Matrix{Char})::Nothing
+    i::Int = 1
+    newPos::Pos = (0, 0)
+    while i <= N_MOLECULES
+        newPos = moveMolecule(molecules![i])
+        if isWithinContainer(newPos, container)
+            molecules![i] = newPos
+            i += 1
+        end
+    end
+    return nothing
+end
+
+function clearLines(nLines::Int)::Nothing
+    @assert 0 < nLines "nLines must be a positive integer"
+    # "\033[xxxA" - xxx moves cursor up xxx LINES
+    print("\033[" * string(nLines) * "A")
+    # "\033[0J" - clears from cursor position till the end of the screen
+    print("\033[0J")
+    return nothing
+end
+
+# function simulateBrownian!(nSimulations::Int=1_000)::Nothing
+function simulateBrownian!(nSimulations::Int=100)::Nothing
+    # @assert 500 < nSimulations < 1e5 "nSimulations must be in range (500, 1e5)"
+    container::Matrix{Char} = fill(' ', N_ROWS, N_COLS);
+    addBorders!(container)
+    molecules::Vec{Pos} = fill((0, 0), N_MOLECULES)
+    rndPlaceMolecules!(molecules, (2, N_ROWS), (2, roundFlt2int(N_COLS/2)))
+    addMolecules!(molecules, container)
+    printContainer(container)
+
+    for _ in 1:nSimulations
+        clearLines(N_ROWS)
+        clearContainer!(container)
+        moveBrownian!(molecules, container)
+        addMolecules!(molecules, container)
+        printContainer(container)
+        sleep(rand(0.1:0.05:0.2))
     end
     return nothing
 end
@@ -79,14 +140,7 @@ function main()::Nothing
     println("Continue with the simulation? [Y/n]")
     choice::Str = readline()
     if lowercase(strip(choice)) in ["y", "yes", ""]
-
-        container::Matrix{Char} = fill(' ', N_ROWS, N_COLS);
-        molecules::Vec{Pos} = fill((0, 0), N_MOLECULES)
-
-        addBorders!(container)
-        rndPlaceMolecules!(molecules)
-        addMolecules!(molecules, container)
-        printContainer(container)
+        simulateBrownian!()
     end
 
     println("\nThat's all. Goodbye!")
