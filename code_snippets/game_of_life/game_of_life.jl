e823995086b99435b36441d4883554bc60de4ec4
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2026-04-20 17:46:47 +0200

    cosmetic changes in game_of_life.jl

--- code_snippets/game_of_life/game_of_life.jl
+++ code_snippets/game_of_life/game_of_life.jl
@@ -4,30 +4,31 @@
 # TODO: game of life
 # https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life
 
-const WIDTH = 80
-const HEIGHT = 15
+const N_COLS = 80
+const N_ROWS = 40
 const PROB_ALIVE = 0.25
-const ALIVE_SYMBOL = "O"
-const DEAD_SYMBOL = "."
+const ALIVE_SYMBOL = '#'
+const DEAD_SYMBOL = '.'
 
 const Universe = Matrix{Bool}
 
 function getEmptyUniverse()::Universe
-    return zeros(Bool, HEIGHT, WIDTH)
+    return zeros(Bool, N_ROWS, N_COLS)
 end
 
 function getRandUniverse()::Universe
-    return rand(Float64, HEIGHT, WIDTH) .<= PROB_ALIVE
+    return rand(Float64, N_ROWS, N_COLS) .<= PROB_ALIVE
 end
 
-function getFieldSymbol(field::Bool)::String
+function getFieldSymbol(field::Bool)::Char
     return field ? ALIVE_SYMBOL : DEAD_SYMBOL
 end
 
-function printUniverse(u::Universe, cycleNum::Int)
+function printUniverse(u::Universe, cycleNum::Int)::Nothing
     population::Int = sum(u)
-    for r in 1:HEIGHT
+    println("cycle: $cycleNum, population: $population")
+    for r in 1:N_ROWS
         println(map(getFieldSymbol, u[r, :]) |> join)
     end
-    println("cycle: $cycleNum, population: $population")
+    return nothing
 end
